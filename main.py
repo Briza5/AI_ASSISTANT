ab701e0204c89b3e0c99d54dee4b3484a155997e
Briza5 <BrizaL555@gmail.com>
2025-10-26 14:58:24 +0100

    add while loop for conversetion flow and context of discusion (python way)

--- main.py
+++ main.py
@@ -7,18 +7,33 @@ load_dotenv()  # Load environment variables from a .env file
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+
+system_prompt = """Jseš Albert Einstein, slavný fyzik známý pro svou teorii relativity.
+Jsi velmi přátelský a ochotný pomoci s jakýmkoli dotazem týkajícím se vědy nebo života.
+ Tvůj výklad je v zábavné a snadno pochopitelné formě.
+Odpovídej na otázky s humorem a moudrostí, jako bys to dělal skutečný Albert Einstein.
+Nikdy nepřestávej být Albert Einstein, i když se tě někdo zeptá na něco mimo oblast
+ se vědy nebo života. Odpověz maximálně ve 2-3 větách."""
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=GEMINI_API_KEY,
     temperature=0.5
 )
 
-response = llm.invoke([{"role": "user", "content": "Ahoj, jsem Albert Einstein. Jak ti mohu pomoci?"}])
-print(response)
-# print("Ahoj, jsem Albert Einstein. Jak ti mohu pomoci?")
 
-# while True:
-#     user_input = input("Ty: ")
-#     if user_input == "exit":
-#         break
-#     print(f"Albert Einstein: Zajímavé, řekl jsi {user_input}.")
+
+
+print("Ahoj, jsem Albert Einstein. Jak ti mohu pomoci?")
+
+history = []
+
+while True:
+    user_input = input("Ty: ")
+    if user_input == "exit":
+        break
+    history.append({"role": "user", "content": user_input})
+    # Vytvoření konverzačního promptu s rolí systému a uživatele
+    response = llm.invoke([{"role": "system", "content": system_prompt}] + history) 
+    print(f"Albert Einstein: {response.content}")
+    history.append({"role": "assistant", "content": response.content})
